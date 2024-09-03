6f6825cbed6805f67896c4a84b437e76dce1728a
George M <gmeyer49s@gmail.com>
2024-09-03 13:10:08 -0700

    Add files via upload

--- app/posts/page.js
+++ app/posts/page.js
@@ -1,33 +1,42 @@
-import React from 'react'
+// import React from 'react'
 
-const Posts = ({ posts }) => {
-  return (
-    <div>
-      <h1>All Posts</h1>
-      <ul>
-        {posts.map((post) => (
-          <li key={post.id}>
-            <h2>{post.title}</h2>
+// const Posts = ({ posts }) => {
+//   return (
+//     <div>
+//       <h1>All Posts</h1>
+//       <ul>
+//         {posts.map((post) => (
+//           <li key={post.id}>
+//             <h2>{post.title}</h2>
            
-          </li>
-        ))}
-      </ul>
-    </div>
-  );
-}
-export  async function PostsPage() {
+//           </li>
+//         ))}
+//       </ul>
+//     </div>
+//   );
+// }
+// export  async function PostsPage() {
   
-    const res = await fetch('http://localhost:3000/api/posts')
-    const posts = await res.json();
+//     const res = await fetch('http://localhost:3000/api/posts')
+//     const posts = await res.json();
   
    
  
-    return {
-      props: {
-        posts,
-      }
-    };
+//     return {
+//       props: {
+//         posts,
+//       }
+//     };
   
-}
+// }
 
-export default Posts;
+// export default Posts;
+import React from 'react'
+
+export default function PostsPage() {
+  return (
+    <div>
+      <div>Posts</div>
+    </div>
+  )
+}
